76fe9e41ad516a36a976529492f01c75981b2493
wowTerrific <marshallstutzman@gmail.com>
2021-02-09 14:09:14 -0500

    initial

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,9 +39,7 @@ const Layout = ({ children }) => {
             marginTop: `2rem`,
           }}
         >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+	  <p>Get on it!</p>
         </footer>
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,42 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Duk from "../images/duk-min.png"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+	<h3>WebDev</h3>
+		<ul>
+			<li>MERN Template</li>
+			<li>Gatsby Template</li>
+		</ul>
+	<h3>Fitness</h3>
+		<ul>
+			<li>Strength Training MWF</li>
+			<li>Cardio on Rest days</li>
+			<li>Mobility daily</li>
+		</ul>
+	<h3>Nutrition</h3>
+		<ul>
+			<li>0.82 grams of protein / 1 lb of bodyweight</li>
+			<li>160 lbs bodyweight</li>
+		</ul>
+	<h3>Meditation</h3>
+		<ul>
+			<li>20 Minutes Daily</li>
+		</ul>
+	<h3>Reading</h3>
+		<ul>
+			<li>20 Minutes Before Bed</li>
+		</ul>
+	<h3>Finance</h3>
+		<ul>
+			<li>Save $5k by 1/1/22</li>
+		</ul>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <img src={Duk} alt="duk" style={{width: "100%"}} />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
